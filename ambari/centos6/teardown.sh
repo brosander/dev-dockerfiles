66807b34a7a39070bbf9275ab8732faefb73b8c8
Bryan Rosander <bryanrosander@gmail.com>
2016-06-28 13:20:10 -0400

    adding teardown

---
+++ ambari/centos6/teardown.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+exitOnFail() {
+  "$@"
+  if [ $? -ne 0 ]; then
+    echo "Command: $@ failed"
+    exit 1
+  fi
+}
+
+killContainer() {
+  if [ -n "`docker ps | awk '{print $NF}' | grep \"^$1\$\"`" ]; then
+    echo "Killing $1"
+    exitOnFail docker kill "$1"
+  else
+    echo "$1 not running so not killing"
+  fi
+}
+
+rmContainer() {
+  if [ -n "`docker ps -a | awk '{print $NF}' | grep \"^$1\$\"`" ]; then
+    echo "Removing $1"
+    exitOnFail docker rm "$1"
+  else
+    echo "$1 doesn't exist so not removing"
+  fi
+}
+
+killAndRemoveContainer() {
+  killContainer "$1"
+  rmContainer "$1"
+}
+
+killAndRemoveContainer ambari-ssh-gateway
+killAndRemoveContainer ambari
+
+for i in `docker ps | awk '{print $NF}' | grep "^centos6"`; do
+  killAndRemoveContainer "$i"
+done
+
+for i in `docker ps -a | awk '{print $NF}' | grep "^centos6"`; do
+  rmContainer "$i"
+done
+
+if [ "killsquid" = "$1" ]; then
+  killAndRemoveContainer squid
+else
+  echo "If you want to kill and remove your squid container as well, use killsquid as the value for arg 1"
+fi
