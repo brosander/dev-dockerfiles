a88684a349e0d2004bd6cece166ce2c95d150d08
Bryan Rosander <bryanrosander@gmail.com>
2016-07-03 15:41:56 -0400

    nifi archive support

--- nifi/ubuntu/start.sh
+++ nifi/ubuntu/start.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+ZIP_FILE="$(find /opt/nifi-archive/ -maxdepth 1 -name 'nifi*.zip' | head -n 1)"
+
 if [ -e "/opt/nifi/bin/nifi.sh" ]; then
-  tail -f /opt/nifi/logs/* &
-  /opt/nifi/bin/nifi.sh run
+  echo "Using nifi installation mounted at /opt/nifi"
+elif [ -n "$ZIP_FILE" ]; then
+  echo "Using nifi archive $ZIP_FILE"
+  unzip -d ~/ "$ZIP_FILE"
+  SCRIPT="`find ~/ -name nifi.sh`"
+  NIFI_DIR="`dirname \"$SCRIPT\"`/.."
+  cd "$NIFI_DIR" && mv * /opt/nifi/
 else
   echo "Must mount nifi installation at /opt/nifi"
+  exit 1
 fi
 
+/opt/nifi/bin/nifi.sh run
