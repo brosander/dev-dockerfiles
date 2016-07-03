836e4bb9d03bb80516fb600d0a7168d896c4f778
Bryan Rosander <bryanrosander@gmail.com>
2016-07-03 14:19:52 -0400

    nifi, minifi updates

--- minifi/ubuntu/start.sh
+++ minifi/ubuntu/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -e "/opt/nifi/bin/nifi.sh" ]; then
+if [ -e "/opt/minifi/bin/minifi.sh" ]; then
   /opt/minifi/bin/minifi.sh start
 else
   echo "Must mount minifi installation at /opt/minifi"
--- nifi/ubuntu/start.sh
+++ nifi/ubuntu/start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 if [ -e "/opt/nifi/bin/nifi.sh" ]; then
+  tail -f /opt/nifi/logs/* &
   /opt/nifi/bin/nifi.sh run
 else
   echo "Must mount nifi installation at /opt/nifi"
 fi
+
