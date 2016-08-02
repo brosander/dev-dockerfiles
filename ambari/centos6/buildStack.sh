b31b8260513398e274d57c91f9b30303fd8b7d33
Bryan Rosander <bryanrosander@gmail.com>
2016-08-02 15:47:38 -0400

    adding no-cache to ambari build script

--- ambari/centos6/buildStack.sh
+++ ambari/centos6/buildStack.sh
@@ -2,7 +2,7 @@
 
 BUILD_DIR="`pwd`"
 if [ -z "$1" ]; then
-  docker build -t ambari .
+  docker build --no-cache -t ambari .
 else
-  docker build --build-arg repo="$1" -t ambari .
+  docker build --no-cache --build-arg repo="$1" -t ambari .
 fi && cd "$BUILD_DIR/../../squid/centos6/" && docker build -t squid . && cd "$BUILD_DIR/../../openssh-server/centos6/" && docker build -t centos6-ssh .
