1646abc0838e22e5ede77fef54fb815fd751a92c
Bryan Rosander <bryanrosander@gmail.com>
2016-08-19 20:32:02 -0500

    Using custom repo arg for non-root agent also

--- ambari/server/centos6/buildStack.sh
+++ ambari/server/centos6/buildStack.sh
@@ -5,4 +5,9 @@ if [ -z "$1" ]; then
   docker build --no-cache -t ambari .
 else
   docker build --build-arg repo="$1" -t ambari .
-fi && cd "$BUILD_DIR/../../../squid/centos6/" && docker build -t squid . && cd "$BUILD_DIR/../../../openssh-server/centos6/" && docker build -t centos6-ssh . && cd "$BUILD_DIR/../../agent/centos6" && docker build -t non-root-ambari-agent .
+fi && cd "$BUILD_DIR/../../../squid/centos6/" && docker build -t squid . && cd "$BUILD_DIR/../../../openssh-server/centos6/" && docker build -t centos6-ssh . && cd "$BUILD_DIR/../../agent/centos6" && \
+if [ -z "$1" ]; then
+  docker build -t non-root-ambari-agent .
+else
+  docker build --build-arg repo="$1" -t non-root-ambari-agent .
+fi
