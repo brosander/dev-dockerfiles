cd061239c9643093e7a2511ec0360ba51972f289
Bryan Rosander <bryanrosander@gmail.com>
2016-08-25 12:26:05 -0400

    adding traffic violation sample data for postgres

---
+++ sample-data/nyc_traffic_violations/postgres/import.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" << EOS
+create table violations (Plate varchar,State char(2),License_Type varchar,Summons_Number bigint,Issue_Date date,Violation_Time time,Violation varchar,Judgment_Entry_Date date,Fine_Amount money,Penalty_Amount money,Interest_Amount money,Reduction_Amount money,Payment_Amount money,Amount_Due money,Precinct varchar,County varchar,Issuing_Agency varchar,Violation_Status varchar,Summons_Image varchar);
+\copy violations FROM '/tmp/violations' DELIMITER ',' CSV
+EOS
